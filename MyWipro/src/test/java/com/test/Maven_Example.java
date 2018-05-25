105c90b59774c8d791bafea14b09d248a8e073be
Hari-06 <hariharan.hari6@gmail.com>
2018-05-25 23:24:18 +0530

    updated extent report

--- MyWipro/src/test/java/com/test/Maven_Example.java
+++ MyWipro/src/test/java/com/test/Maven_Example.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -11,11 +12,16 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 
 
 public class Maven_Example extends Report
 {
-	private RemoteWebDriver driver;		
+	private RemoteWebDriver driver;	
+	Markup m1,m2,m3;
 	@Test				
 	public void Launch_Google() throws Exception 
 	{	
@@ -23,16 +29,50 @@ public class Maven_Example extends Report
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Google"));
 		getScreenshot();
-
-		test.info("details-1", MediaEntityBuilder.createScreenCaptureFromPath("./screenshot.png").build());
+		
+		test.info("html", MediaEntityBuilder.createScreenCaptureFromPath("./screenshot.png").build());
 		//MediaEntityModelProvider mediaModel = MediaEntityBuilder.createScreenCaptureFromPath("./screenshot.png").build();
 	    //test.info("details-2", mediaModel);	
 	    //test.info("details-3").addScreenCaptureFromPath("./screenshot.png");
+		
+		//Table
+		String[][] data = {
+			    { "Step no", "Step Description", "Expected" , "Actual"},
+			    { "Content.1.1", "Content.2.1", "Content.3.1" , "Content.4.1 " },
+			    { "Content.1.2", "Content.2.2", "Content.3.2" , "Content.4.2 " },
+			    { "Content.1.3", "Content.2.3", "Content.3.3" , "Content.4.3 " },
+			    { "Content.1.4", "Content.2.4", "Content.3.4" , "Content.4.4 " }
+			};
+			m1 = MarkupHelper.createTable(data);
+
+			test.pass(m1);
+			// or
+			test.log(Status.PASS, m1);
+			
+			//Label
+			String text = "extent";
+			 m2 = MarkupHelper.createLabel(text, ExtentColor.BLUE);
+
+			test.pass(m2);
+			// or
+			test.log(Status.PASS, m2);
+			
+			
+			//Code Block
+			String code = "\n\t\n\t\tText\n\t\n";
+			Markup m3 = MarkupHelper.createCodeBlock(code);
+
+			test.pass(m3);
+			// or
+			test.log(Status.PASS, m3);
+			
+			
+			
 	}	
 	@BeforeTest
 	public void beforeTest() 
 	{	
-		System.setProperty("webdriver.gecko.driver", "D:\\Hari\\drivers\\geckodriver\\geckodriver_64.exe");
+		System.setProperty("webdriver.gecko.driver", ".//geckodriver_64.exe");
 		driver = new FirefoxDriver();  
 	}		
 	@AfterTest
--- MyWipro/src/test/java/com/test/Report.java
+++ MyWipro/src/test/java/com/test/Report.java
@@ -2,6 +2,8 @@ package com.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -11,11 +13,13 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.AnalysisStrategy;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Protocol;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Report implements ITestListener 
@@ -78,7 +82,7 @@ public class Report implements ITestListener
 	  
 	 }
 	 
-	 public void onStart(ITestContext context) 
+	 public void onStart(ITestContext context)   
 	 {
 		 System.out.println("on start");
 		 htmlReporter = new ExtentHtmlReporter(".//report1.html");
@@ -89,18 +93,55 @@ public class Report implements ITestListener
 		 
 		 //load extent-config file
 		// htmlReporter.loadXMLConfig(".//test-output//extent-config.xml");
-		 
+		 InetAddress ip = null;
+		try {
+			ip = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		 // configure environment info
-		 reports.setSystemInfo("Hostname", "localhost");
+		 reports.setSystemInfo("Username", System.getProperty("user.name"));
+		 reports.setSystemInfo("HostName",ip.getHostName());		 
+		 reports.setSystemInfo("Host Address",ip.getHostAddress());
+		 reports.setSystemInfo("OS", System.getProperty("os.name"));
+		 reports.setSystemInfo("JAVA", System.getProperty("java.version"));
 		 reports.setSystemInfo("Browser", "firefox");
 		 reports.setSystemInfo("Environment", "UAT");
 		 reports.setSystemInfo("Region", "UA1");
 		 
+			
+		 // make the charts visible on report open
 		 htmlReporter.config().setChartVisibilityOnOpen(true);
-		 htmlReporter.config().setDocumentTitle("Automation Testing");
-		 htmlReporter.config().setReportName("My Own Report");
-		 htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+
+		 
+		 // report title
+		 htmlReporter.config().setDocumentTitle("aventstack - ExtentReports");
+
+		 // encoding, default = UTF-8
+		 htmlReporter.config().setEncoding("UTF-8");
+
+		 // protocol (http, https)
+		 htmlReporter.config().setProtocol(Protocol.HTTPS);
+
+		 // report or build name
+		 htmlReporter.config().setReportName("Build-1224");
+
+		 // chart location - top, bottom
+		 htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
+
+		 // theme - standard, dark
 		 htmlReporter.config().setTheme(Theme.STANDARD);
+
+		 // set timeStamp format
+		 htmlReporter.config().setTimeStampFormat("mm/dd/yyyy hh:mm:ss a");
+
+		 // add custom css
+		 htmlReporter.config().setCSS("css-string");
+
+		 // add custom javascript
+		 htmlReporter.config().setJS("js-string");
+		
 	 }
 	 
 	 public void onFinish(ITestContext context) 
